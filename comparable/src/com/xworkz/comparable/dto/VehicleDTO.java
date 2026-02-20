0379c9251f4652ae1f42554ccc3a77b072bf3d60
Sahana052003 <sahananreddy52003@gmail.com>
2026-02-20 21:38:22 +0530

    Comparable

---
+++ comparable/src/com/xworkz/comparable/dto/VehicleDTO.java
@@ -0,0 +1,27 @@
+package com.xworkz.comparable.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VehicleDTO implements Comparable<VehicleDTO> {
+
+//public class VehicleDTO{
+    private Integer id;
+    private String vehicleName;
+    private Long number;
+
+
+    @Override
+    public int compareTo(VehicleDTO vehicleDTO) {
+////        return this.id.compareTo(vehicleDTO.id);
+//       // return vehicleDTO.id.compareTo(this.id);
+////        return this.number.compareTo(vehicleDTO.number);
+//
+////        return this.vehicleName.compareTo(vehicleDTO.vehicleName);
+        return vehicleDTO.vehicleName.compareTo(this.vehicleName);
+    }
+}
---
+++ comparable/src/com/xworkz/comparable/main/VehicleRunner.java
@@ -0,0 +1,50 @@
+package com.xworkz.comparable.main;
+
+import com.xworkz.comparable.dto.VehicleDTO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class VehicleRunner {
+    public static void main(String[] args) {
+        List<VehicleDTO> vehicle=new ArrayList<>();
+
+
+
+        vehicle.add(new VehicleDTO(3,"Suzuki",7682787278L));
+        vehicle.add(new VehicleDTO(67,"Toyoto",78987923L));
+        vehicle.add(new VehicleDTO(16,"Hyundai",76897324L));
+        vehicle.add(new VehicleDTO(5,"kw",5638909789L));
+        vehicle.add(new VehicleDTO(9,"BMW",785676869L));
+
+
+        System.out.println(vehicle);
+        System.out.println(vehicle.size());
+//       vehicle.clear();
+//        System.out.println(vehicle);
+//        vehicle.remove(3);
+//        System.out.println(vehicle);
+
+//        System.out.println(vehicle.remove(4));
+//        System.out.println(vehicle);
+
+
+       Iterator<VehicleDTO> iterator = vehicle.iterator();
+       while (iterator.hasNext()) {
+           //  System.out.println(iterator.next());
+           VehicleDTO vehicleDTO = iterator.next();
+           if (vehicleDTO.getId().equals(5)) {
+               iterator.remove();
+               System.out.println(vehicle);
+           }
+       }
+
+
+
+
+        Collections.sort(vehicle);
+        System.out.println(vehicle);
+    }
+}
