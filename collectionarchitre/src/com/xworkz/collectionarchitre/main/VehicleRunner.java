60327f5e621e689048b789ca36e09527354dee38
Sahana052003 <sahananreddy52003@gmail.com>
2026-03-23 19:39:06 +0530

    collection------->List-->ArrayList Set---->Hashset
    Map------>HashMap

--- collectionarchitre/src/com/xworkz/collectionarchitre/main/VehicleRunner.java
+++ collectionarchitre/src/com/xworkz/collectionarchitre/main/VehicleRunner.java
@@ -6,16 +6,18 @@ import java.util.Map;
 public class VehicleRunner {
     public static void main(String[] args) {
         Map<Vehicle,Integer> map=new HashMap<>();
-        Vehicle vehicle=new Vehicle("Toyota",43212);
+        Vehicle vehicle=new Vehicle("Toyota",432);
         Vehicle vehicle1=new Vehicle("Honda",876);
         Vehicle vehicle2=new Vehicle("suzuki",876);
-        Vehicle vehicle3=new Vehicle("Toyota",9876);
+        Vehicle vehicle3=new Vehicle("Toyota",432);
 
         map.put(vehicle,3456);
         map.put(vehicle1,2345);
         map.put(vehicle2,1234);
-        map.put(vehicle3,23456);
-        System.out.print(map);
+        map.put(vehicle,23456);
+        System.out.println(map);
+
 
     }
 }
+
