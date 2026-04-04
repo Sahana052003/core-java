a5ec3002bd7acaf5df6ab042a434e5b6c6b52d4c
Sahana052003 <sahananreddy52003@gmail.com>
2026-04-04 22:16:15 +0530

    Comparator and Comparable

---
+++ comparator/src/com/xworkz/comparator/Product.java
@@ -0,0 +1,62 @@
+package com.xworkz.comparator;
+
+public class Product implements Comparable<Product>{
+    private Integer id;
+    private String productName;
+    private Double price;
+    private Boolean isAvailable;
+
+    public Product(Integer id, String productName, Double price, Boolean isAvailable) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return this.id.compareTo(o.id);
+    }
+}
---
+++ comparator/src/com/xworkz/comparator/ProductAvailableComparator.java
@@ -0,0 +1,10 @@
+package com.xworkz.comparator;
+
+import java.util.Comparator;
+
+public class ProductAvailableComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        return o1.getAvailable().compareTo(o2.getAvailable());
+    }
+}
