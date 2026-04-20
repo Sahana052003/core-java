a5b18f62659e3b5984a9f2cf01f5c6723284ea70
Sahana052003 <sahananreddy52003@gmail.com>
2026-04-20 20:31:04 +0530

    COLLECTION and MAP

--- collection/src/com/xworkz/collection/runner/CollectionDemo.java
+++ collection/src/com/xworkz/collection/runner/CollectionDemo.java
@@ -9,8 +9,11 @@ public class CollectionDemo {
         list.add(13456);
         list.add(234);
         list.add(234);
-        list.add(null);
+        //list.add(null);
         System.out.println(list);
+        System.out.println("000000000000000000000000000");
+        System.out.println(list.indexOf(234));
+        System.out.println(list.get(2));
 
         List<Integer> list1=new ArrayList<>();
 
@@ -18,8 +21,34 @@ public class CollectionDemo {
         System.out.println(list1.remove(1));
         //System.out.println(list1);
 
-
         list1.retainAll(list);
         System.out.println(list1);
+        System.out.println("--------------------------------");
+        System.out.println(  list.indexOf(345));
+
+
+        ListIterator<Integer> itr = list.listIterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+
+        System.out.println("--------------------------------");
+        Collections.sort(list);
+        System.out.println(list);
+
+
+        System.out.println("ARRAYLIST");
+        List<Integer> list2=new ArrayList<>();
+        list2.add(23456);
+        list2.add(12);
+        list2.add(45);
+        list2.add(8);
+        list2.add(-4);
+        list2.add(23);
+        System.out.println(list2);
+
+        Collections.sort(list2);
+        System.out.println(list2);
     }
 }
