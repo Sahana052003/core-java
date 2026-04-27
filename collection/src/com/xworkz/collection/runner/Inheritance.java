a5ea9c2cbfdb78eeaa1eaa9eaa594ef8689ab0c4
Sahana052003 <sahananreddy52003@gmail.com>
2026-04-27 13:44:48 +0530

    Inheritance

---
+++ collection/src/com/xworkz/collection/runner/Inheritance.java
@@ -0,0 +1,8 @@
+package com.xworkz.collection.runner;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        VehicleInheritance vehicleInheritance=new Car();
+        vehicleInheritance.start();
+    }
+}
